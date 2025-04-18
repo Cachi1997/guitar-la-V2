7f4fa125677c9cb08dadf26ed0e823eb9ed6c0f2
cachi1997 <cachigaliano1997@gmail.com>
2025-04-18 12:44:16 -0300

    Remove "Cachi" from the copyright notice in the Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-dark mt-5 py-5">
       <div className="container-xl">
         <p className="text-white text-center fs-4 mt-4 m-md-0">
-          GuitarLA - Todos los derechos Reservados Cachi
+          GuitarLA - Todos los derechos Reservados
         </p>
       </div>
     </footer>
